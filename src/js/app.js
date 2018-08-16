bb92238ba00e66d63ab11dfcef37236b8f1728a5
Wilker Viana <wilker.info@gmail.com>
2018-08-16 00:01:02 -0300

    Add interation menu hamburguer mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-// 'use strict';
+// "use strict";
 
 // window.onscroll = scrollMenu;
 // window.onload = onLoad;
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
+"use strict";
+
+(function showMenu() {
+  let menu = document.querySelector(".main-nav");
+  let menuHamburguer = document.querySelectorAll(".hamburguer");
+  menuHamburguer.forEach(btn => {
+    btn.addEventListener("click", () => {
+      btn.classList.toggle("is-active");
+      menu.classList.toggle("is-active");
+    });
+  });
+})();
+
 console.log("It's alive! :D");
